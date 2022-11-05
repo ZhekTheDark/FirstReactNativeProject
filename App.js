23aa8c5fea66122fe6ec9ff64536fb3ea507a35f
ZhekTheDark <Zhekzerg@gmail.com>
2022-11-05 23:14:30 +0300

    test

--- App.js
+++ App.js
@@ -1,130 +1,18 @@
 import React from 'react';
 import {Text, TextInput, View} from 'react-native';
 
-// function Greeting() {
-//   return <Text>Hello, world!</Text>;
-// }
-// const Greeting = () => <Text>Hello, world!</Text>;
-
-// function GreetingWithFlex(props) {
-//     return <Text style={{
-//         flex: parseInt(props.flex),
-//         backgroundColor: "#a9a9a9"
-//     }}>
-//         Hello, world!
-//     </Text>;
-// }
-
-// const HelloWorldApp = () => {
-//     // const App = () => {
-//     return (
-//         <View
-//             style={{
-//                 flex: 1,
-//                 justifyContent: "center",
-//                 alignItems: "center"
-//             }}>
-//             <Greeting/>
-//             <Greeting/>
-//             <GreetingWithFlex flex="3"/>
-//         </View>
-//     )
-// }
-
-// function Welcome(props) {
-//   return (
-//     // <View>
-//     // <Text>Hello, {props.name}</Text>
-//     <Text>Hello, {props.name}</Text>
-//     // </View>
-//   )
-// }
-
-// function HelloWorldApp() {
-//   return (
-//     <View>
-//       <Welcome name="Sara" />
-//       <Welcome name="Cahal" />
-//       <Welcome name="Edite" />
-//     </View>
-//   )
-// }
-
-// const HelloWorldApp = () => {
-//   return (
-//     <ScrollView>
-//       <Text>Some Text</Text>
-//       <View>
-//         <Text>Some more Text</Text>
-//         <Image
-//           source={{uri: 'https://reactnative.dev/docs/assets/p_cat2.png'}}
-//           style={{width: 200, height: 200}}
-//         />
-//       </View>
-//       <TextInput
-//         style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-//         defaultValue="You can type in me"
-//       />
-//     </ScrollView>
-//   );
-// };
-//
-// export default HelloWorldApp;
-
-// const getFullName = (firstName, secondName, thirdName) => {
-//   return `${firstName} ${secondName} ${thirdName}`;
-// };
-//
-// const Cat = () => {
-//   return <Text>Hello, I am {getFullName('Rum', 'Tum', 'Tugger')}!</Text>;
-// };
-
-//const Cat = () => {
-//  return (
-//    <View>
-//      <Text>Hello, I am...</Text>
-//      <TextInput
-//        style={{
-//          height: 40,
-//          borderColor: 'gray',
-//          borderWidth: 1,
-//        }}
-//        defaultValue="Name me!"
-//      />
-//    </View>
-//  );
-//};
-
-//const Cat = () => {
-//    return (
-//        <View>
-//            <Text>I am also a cat!</Text>
-//        </View>
-//    );
-//};
-//
-//const Cafe = () => {
-//    return (
-//        <View>
-//            <Text>Welcome!</Text>
-//            <Cat/>
-//            <Cat/>
-//            <Cat/>
-//        </View>
-//    );
-//};
-
 const Cat = props => {
   return (
     <View>
-      <Text>Hello, I am {props.name}!</Text>
+      {/* <Text>Hello, I am {props.name}!</Text> */}
+      <Text>asdbfhasbdfhabsfdhbhsdf {props.name}!</Text>
     </View>
   );
 };
 
 const Cafe = () => {
   return (
-    <View>
+    <View style={styles.containerMain}>
       <Cat name="Maru" />
       <Cat name="Jellylorum" />
       <Cat name="Spot" />
@@ -132,4 +20,15 @@ const Cafe = () => {
   );
 };
 
-export default Cat;
+const styles = StyleSheet.create({
+  containerMain: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#F5FCFF',
+    width: null,
+    height: null,
+  },
+});
+
+export default Cafe;

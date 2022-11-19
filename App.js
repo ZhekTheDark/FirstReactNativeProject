8c2cde8bd25c832181ed687a0685aeb8bc39d08b
e.bobrutskov <e.bobrutskov@tinkoff.ru>
2022-11-19 22:17:23 +0300

    state lesson

--- App.js
+++ App.js
@@ -1,19 +1,35 @@
-import React from 'react';
-import {StyleSheet, Text, View, Image} from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Button, Text, View, Image } from 'react-native';
 
-const CatApp = () => {
-    return (
-        <View>
-            <Image
-                source={{uri: "https://reactnative.dev/docs/assets/p_cat1.png"}}
-                style={{width: 200, height: 200}}
-            />
-            <Text>Hello, I am your cat!</Text>
-        </View>
-    );
+const Cat = (props) => {
+  const [isHungry, setIsHungry] = useState(true);
+
+  return (
+    <View>
+      <Text>
+        I am {props.name}, and I am {isHungry ? "hungry" : "full"}!
+      </Text>
+      <Button
+        onPress={() => {
+          setIsHungry(false);
+        }}
+        disabled={!isHungry}
+        title={isHungry ? "Pour me some milk, please!" : "Thank you!"}
+      />
+    </View>
+  );
 }
 
-export default CatApp;
+const Cafe = () => {
+  return (
+    <>
+      <Cat name="Munkustrap" />
+      <Cat name="Spot" />
+    </>
+  );
+}
+
+export default Cafe;;
 
 const styles = StyleSheet.create({
     containerMain: {
